ab4aa5bd292a45d7033a68b7d678c4d29b21b76c
omar nazih <omarnazihcs@gmail.com>
2023-12-23 02:32:38 +0200

    new page - faq section

---
+++ src/pages/faq.tsx
@@ -0,0 +1,16 @@
+import Layout from "@/layouts/layout";
+
+const FaqPage = () => {
+  return (
+    <Layout>
+      <h1>FAQ</h1>
+      <ul>
+        <li>What is this?</li>
+        <li>Why is this?</li>
+        <li>How is this?</li>
+      </ul>
+    </Layout>
+  );
+};
+
+export default FaqPage;
